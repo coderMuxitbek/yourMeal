2a889eb458106e0f86670c31c6100553be39196b
Muxitbek <codermuxitbek@gmail.com>
2026-02-16 23:43:11 +0500

    gonna deploy 4

--- server/app.js
+++ server/app.js
@@ -3,11 +3,24 @@ const app = express();
 const cors = require("cors");
 const MealsRouter = require("./Routes/mealsRouter.js");
 const qs = require("qs");
+const path = require("path");
 
-app.use(cors());
+const _dirname = path.resolve();
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors());
+}
 app.use(express());
 app.set("query parser", "extended");
+app.use("/", MealsRouter);
 
-app.use("/", MealsRouter)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(_dirname, "../client/dist")));
+
+    app.get("/{*splat}", (req, res) => {
+        res.sendFile(path.join(_dirname, "..client", "dist", "index.html"));
+    })
+}
 
 module.exports = app;
