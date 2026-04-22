7880f2588e81d55d5c7d57f2bbe3136f85e03a70
Muxitbek <codermuxitbek@gmail.com>
2026-04-22 20:07:28 +0500

    address

--- client/src/Components/GetAddress.jsx
+++ client/src/Components/GetAddress.jsx
@@ -7,6 +7,7 @@ function GetAddress({ SetAskAddress }) {
     const [howOrder, SetHowOrder] = useState("Delivery");
     const [address, SetAddress] = useState({});
     const [suggestions, SetSuggestions] = useState([]);
+    const [userStreet, SetUserStreet] = useState("");
 
     const GetInputData = (e) => {
         const name = e.target.name;
@@ -17,6 +18,15 @@ function GetAddress({ SetAskAddress }) {
         })
     }
 
+    const GetStreet = (value) => {
+        SetAddress((prev) => {
+            return { ...prev, street: value }
+        })
+
+        SetSuggestions([])
+        console.log(address);
+    }
+
     const SaveAddress = () => {
         localStorage.setItem("USER_ADDRESS_YOURMEAL", JSON.stringify(address));
         SetAskAddress(false);
@@ -24,10 +34,8 @@ function GetAddress({ SetAskAddress }) {
     }
 
     useEffect(() => {
-        // if (address.street.length < 3) return;
-
         const getData = setTimeout(async () => {
-            axios.get(`https://api.geoapify.com/v1/geocode/autocomplete?text=${address.street}&apiKey=0993f9a905d24db6a70999d20ec52fe8&filter=countrycode:uz`)
+            axios.get(`https://api.geoapify.com/v1/geocode/autocomplete?text=${userStreet}&apiKey=0993f9a905d24db6a70999d20ec52fe8&filter=countrycode:uz`)
                 .then((res) => {
                     console.log(res);
                     SetSuggestions(res.data.features);
@@ -35,9 +43,7 @@ function GetAddress({ SetAskAddress }) {
                     console.log(err);
                 })
         }, 400);
-
-        // return clearTimeout(getData);
-    }, [address.street])
+    }, [userStreet])
 
     return (
         <div className="md:w-full md:h-full md:fixed top-0 left-0 bg-[#00000099] flex">
@@ -57,18 +63,18 @@ function GetAddress({ SetAskAddress }) {
                             </div>
 
                             {howOrder === "Delivery" && <div className="flex flex-col gap-2">
-                                <div>
-                                    <input onChange={(e) => GetInputData(e)} name="street" className="w-full md:w-75 lg:w-full h-7.5 lg:h-10 border pl-3 rounded-lg text-[10px] lg:text-[12px] leading-[130%] text-[#B1B1B1]" type="text" placeholder="Улица, дом, квартира" />
-                                    <ul className="flex flex-col gap-1">
+                                <div className="w-full md:w-75 lg:w-full flex flex-col gap-1 relative">
+                                    <input onChange={(e) => SetUserStreet(e.target.value)} value={address.street} name="street" className="w-full md:w-75 lg:w-full h-7.5 lg:h-10 border pl-3 rounded-lg text-[10px] lg:text-[12px] leading-[130%] text-[#B1B1B1]" type="text" placeholder="Улица, дом, квартира" />
+                                    {suggestions.length > 0 && <ul className="w-full max-h-40 bg-amber-50 border rounded-lg absolute top-8 md:top-9 lg:top-11 z-10 overflow-auto">
                                         {suggestions.map((place, i) => {
-                                            return <li key={i}>
+                                            return <li onClick={() => GetStreet(place.properties.formatted)} className="hover:bg-amber-200 px-3" key={i}>
                                                 <p>{place.properties.formatted}</p>
                                                 <hr />
                                             </li>
                                         })}
-                                    </ul>
+                                    </ul>}
                                 </div>
-                                <div className="w-full md:w-75 lg:w-full flex justify-between text-[10px] lg:text-[12px] leading-[130%] text-[#B1B1B1]">
+                                <div className="w-full md:w-75 lg:w-full flex justify-between text-[10px] lg:text-[12px] leading-[130%] text-[#B1B1B1] z-5">
                                     <input onChange={(e) => GetInputData(e)} name="floor" className="w-[45%] h-7.5 lg:h-10 border pl-3 rounded-lg" type="text" placeholder="Этаж" />
                                     <input onChange={(e) => GetInputData(e)} name="doorPhone" className="w-[45%] h-7.5 lg:h-10 border pl-3 rounded-lg" type="text" placeholder="Домофон" />
                                 </div>
@@ -96,7 +102,7 @@ function GetAddress({ SetAskAddress }) {
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
